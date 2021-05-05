7be2f0e5fd454031a329bc952779f8a00cb849a2
axmaxon <axmaxon@gmail.com>
2021-05-05 21:18:02 +0500

    configure: webpack | add: bootstrap

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,8 +1,12 @@
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
+// a relevant structure within app/frontend and only use these pack files to reference
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+import "../styles/application.scss"
+import 'bootstrap/dist/js/bootstrap.js'
+
+const images = require.context('../images/', true)
 
 Rails.start()
