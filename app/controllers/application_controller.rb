1f57990f780e4c383675bac79be2cdbd4e2de54b
axmaxon <axmaxon@gmail.com>
2021-05-12 12:31:08 +0500

    add: Devise to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  # Настройка для работы Девайза, когда юзер правит профиль
+  # Если в данный момент выполняется девайсовский контроллер разрешаем параметры
+  # для обновления регистрации пользователя (то же что делаем в каждом контроллере, но
+  # специально для девайса)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: [:password, :password_confirmation, :current_password]
+    )
+  end
 end
