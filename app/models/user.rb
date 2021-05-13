bad8307d4850e77497f9eb058a7a957e3e8ab007
axmaxon <axmaxon@gmail.com>
2021-05-13 11:30:38 +0500

    configure: the Devise | change: links in the views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :events
 
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
+  # При создании нового пользователя, перед валидацией объекта выполнить метод set_name
+  before_validation :set_name, on: :create
+
+  private
+
+  # Задаем юзеру случайное имя, если оно пустое
+  def set_name
+    self.name = "инкогнито №#{rand(777)}" if self.name.blank?
+  end
 end
