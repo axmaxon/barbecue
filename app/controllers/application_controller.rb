959a55154d3e67a91138263506defdf537d5ca16
axmaxon <axmaxon@gmail.com>
2021-05-13 12:46:50 +0500

    add: authorization in EventsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,21 @@ class ApplicationController < ActionController::Base
   # специально для девайса)
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # Хелпер будет доступен во всех вьюхах
+  helper_method :current_user_can_edit?
+
+  # Настройка для девайза — разрешаем обновлять профиль, но обрезаем
+  # параметры, связанные со сменой пароля.
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :account_update,
       keys: [:password, :password_confirmation, :current_password]
     )
+  end
+
+  # Вспомогательный метод, возвращает true, если текущий залогиненный юзер
+  # может править указанное событие
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
   end
 end
