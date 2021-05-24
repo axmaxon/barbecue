e64d467eaa2580d29b8d847614ab1d22dd978f76
axmaxon <axmaxon@gmail.com>
2021-05-24 09:32:10 +0500

    add: comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
   root "events#index"
-  resources :events
+
+  resources :events do
+    # Вложенный ресурс комментариев
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :edit, :update]
 end
