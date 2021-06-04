0552e986367ed6e7b2d23577cfe7eff3e14ebdc9
axmaxon <axmaxon@gmail.com>
2021-06-04 09:37:23 +0500

    fix: to destroy of event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,11 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
   # Чтобы Рельсы понимали, что именно мы дергаем через subscribers, надо указать source
   has_many :subscribers, through: :subscriptions, source: :user
 
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
-  validates :user, presence: true
 end
