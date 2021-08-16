b326bcba6e2cc093571f2ee2da028422ff76e6fc
axmaxon <axmaxon@gmail.com>
2021-08-16 10:22:46 +0500

    add: the route for POST query

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
     resources :subscriptions, only: [:create, :destroy]
     resources :photos, only: [:create, :destroy]
+
+    # Возможность передать POST-запрос по уже существующему адресу: events/show (юзер
+    # передает пин-код)
+    post :show, on: :member
   end
 
   resources :users, only: [:show, :edit, :update]
