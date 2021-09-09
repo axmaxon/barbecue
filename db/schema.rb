790442d17948081c200fccaac6f9b628804e9043
axmaxon <axmaxon@gmail.com>
2021-09-09 14:47:09 +0500

    change: routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_050239) do
+ActiveRecord::Schema.define(version: 2021_09_09_092850) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2021_08_16_050239) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "provider"
+    t.string "url"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
