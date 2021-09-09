dd183810346186e74d54087975e5ab0e6d16692c
axmaxon <axmaxon@gmail.com>
2021-09-09 16:38:37 +0500

    add: find_for_facebook_oauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
-         :validatable, :confirmable, :omniauthable, omniauth_providers: %i[facebook]
+         :validatable, :confirmable, :omniauthable, omniauth_providers: [:facebook]
 
   # Смонтируем загрузчик (указываем подответственное поле таблицы и класс загрузчика )
   mount_uploader :avatar, AvatarUploader
@@ -14,6 +14,28 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 35 }
 
   after_commit :link_subscriptions, on: :create
+
+  def self.find_for_facebook_oauth(access_token)
+    # Достаём email из токена
+    email = access_token.info.email
+    user = where(email: email).first
+
+    # Возвращаем, если нашёлся
+    return user if user.present?
+
+    # Если не нашёлся, достаём провайдера, айдишник и урл
+    provider = access_token.provider
+    id = access_token.extra.raw_info.id
+    url = "https://facebook.com/#{id}"
+
+    # Теперь ищем в базе запись по провайдеру и урлу
+    # Если есть, то вернётся, если нет, то будет создана новая
+    where(url: url, provider: provider).first_or_create! do |user|
+      # Если создаём новую запись, прописываем email и пароль
+      user.email = email
+      user.password = Devise.friendly_token.first(16)
+    end
+  end
 
   private
 
